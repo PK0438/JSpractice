7e97be8fa79d8dfbe7ba8fda59010876850eb88e
Pramod Kumar <pk.1512963@gmail.com>
2024-06-29 21:38:07 -0400

    changes to the rock paper scissors code

---
+++ 01 practice/practice.js
@@ -0,0 +1,23 @@
+//falsy values - false, 0, null, NaN, undefined
+
+let cartQuantity = 0;
+
+if(cartQuantity) {
+  console.log("Cart has Products");
+} else {
+  console.log("Cart is empty");
+}
+
+
+//tarnary operator
+cartQuantity ? console.log("Cart has Products") : console.log("Cart is empty");
+
+
+calculateTax = (cartAmt, taxPercent) => {
+  cartAmt > 1999 ? taxPercent = 1 : taxPercent;
+  console.log(cartAmt * taxPercent);
+}
+
+calculateTax(2001, 0.25);
+
+calculateTax(1500, 0.15);
